d27189071e9037fd1c032b301cb446f5f6581159
Carlos Banuelos <carlosbanuelos174@gmail.com>
2017-06-22 13:16:43 -0400

    added loops and arrays

---
+++ JavaScriptBasics/0.07_loops.js
@@ -0,0 +1,36 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+for (var i = 1; i < 101; i++){
+	console.log(i);
+}
+
+for (var i = 5; i < 11; i++){
+	console.log(i);
+}
+
+for (var i = 10; i > 0; i--){
+	console.log(i);
+}
+
+i++
+
+i = i + 1
+
+i += 5
+
+for (var i=0; i < 30; i += 5){
+	console.log(i)
+}
+
+for (var i = 0; i < 105; i += 5){
+	if(i === 50){
+		console.log("fifty")
+	}
+	else{
+		console.log(i)
+	}
+}
---
+++ JavaScriptBasics/0.08_arrays.js
@@ -0,0 +1,19 @@
+
+var movies = ["Doctor Strange", "Back to the Future", "Mission Impossible", "Guardians of the Galaxy"]
+
+var mixedArray = ["broccoli", 20, true];
+
+console.log(movies[0]);
+
+console.log(mixedArray[0]);
+
+
+for(var i = 0; i < movies.length; i++){
+	console.log(movies[i])
+}
+
+var favoriteRestaurants = ["Red Robin", "McAlister's", "Gyro King", "Mug N' Bun", ]
+
+for(var i = 0; i < favoriteRestaurants.length; i++){
+	console.log(favoriteRestaurants[i])
+}
